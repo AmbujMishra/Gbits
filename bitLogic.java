c75961bb935dd60511b6231cd905f9bc2a22f033
Ambuj <hiambuj4u@gmail.com>
2014-12-18 19:11:27 +0530

    GbitsDesktop bitLogic.java file
    office version

---
+++ bitLogic.java
@@ -0,0 +1,12 @@
+package com.kingAm.games;
+
+public class bitLogic {
+
+	public String processbitSequence(String bitseq) {
+		
+		return bitseq;
+		// TODO Auto-generated method stub
+		
+	}
+// write logic for bit crashing
+}
