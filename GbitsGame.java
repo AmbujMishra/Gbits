60a72761e848af8a6ce6083ca1a0dcf00e4961f2
Ambuj <hiambuj4u@gmail.com>
2014-12-29 18:48:07 +0530

    Create GbitsGame.java

---
+++ GbitsGame.java
@@ -0,0 +1,72 @@
+package com.gameScreens.games;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GbitsGame extends Game{
+	
+	public enum Gravity
+	{
+		ZERO,
+		UP,
+		DOWN,
+		RIGHT,
+		LEFT;
+	}
+	private Gravity g;
+	//https://code.google.com/p/libgdx-users/wiki/ScreenAndGameClasses
+	MainMenuScreen MMS;
+	InputScreen INS;
+	AnimaScreen ANS;
+	GameOverScreen GOS;
+	BitContainer BC;
+	BitProcessing BP;
+	public SpriteBatch batch;
+    public BitmapFont font;
+	public Texture tbitI0;
+	public Texture tbitI1;
+	public FPSLogger fpslog;
+    public void create() {
+    	MMS =new MainMenuScreen(this);
+    	INS=new InputScreen(this);
+    	ANS =new AnimaScreen(this);
+    	GOS= new GameOverScreen(this);
+    	//initializing gravity with ZERO
+    	setGravity(Gravity.ZERO);
+    	BP=new BitProcessing();
+    	batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        //loading texture
+      	tbitI0=new Texture(Gdx.files.internal("bit0.png"));	
+      	tbitI1=new Texture(Gdx.files.internal("bit1.png"));
+      	fpslog=new FPSLogger();
+        setScreen(MMS);
+    }
+
+    public void render() {
+        super.render(); //important!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+        tbitI0.dispose();
+        tbitI1.dispose();
+		Gdx.app.exit();
+    }
+	
+	public void setGravity(Gravity g)
+	{
+		this.g=g;
+	}
+	
+	public Gravity getGravity()
+	{
+		return g;
+	}
+}
