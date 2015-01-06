9c6e6ba0aa3909baa65bb08359de33e6db4919f1
Ambuj <hiambuj4u@gmail.com>
2015-01-06 16:05:14 +0530

    Update InputScreen.java

--- InputScreen.java
+++ InputScreen.java
@@ -23,7 +23,7 @@ public class InputScreen implements Screen,InputProcessor{
 
 	@Override
 	public void show() {
-		game.camera.setToOrtho(false,64*game.BC.getRow(), 64*game.BC.getColumn());
+		game.camera.setToOrtho(false,64*game.BC.getColumn(), 64*game.BC.getRow());
 	    game.camera.update();
 	    
 		if (game.BC.getBitCount()==2)
