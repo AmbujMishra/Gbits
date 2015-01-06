969fe8965da30177e28e1b79717e88b73ecf00fa
Ambuj <hiambuj4u@gmail.com>
2015-01-06 16:03:57 +0530

    Update MainMenuScreen.java

--- MainMenuScreen.java
+++ MainMenuScreen.java
@@ -5,7 +5,7 @@ package com.Gbits.Screens;
  */
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Input.Keys;
+//import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
  
 public class MainMenuScreen implements Screen{
@@ -26,35 +26,45 @@ public class MainMenuScreen implements Screen{
 		game.batch.begin();
 		game.font.draw(game.batch, "Welcome to Gbits!!! ", 100, 400);
 		game.font.draw(game.batch, "Tap anywhere to begin!", 100, 350);
-		game.font.draw(game.batch, "Press 2,3,4 or 5 to set stage", 100, 200);
+		//game.font.draw(game.batch, "Press 2,3,4 or 5 to set stage", 100, 200);
 		game.batch.end();
  
 		if (Gdx.input.isTouched()) {
-			game.BC.Stage(22);
-			game.setScreen(game.INS);
+			//System.out.println("touched");
+			/*game.BC.Stage(22);
+		    for (int i=0;i<game.BC.getRow();i++)
+			    System.out.println(game.BC.getBitRow(i));*/
+			game.setScreen(game.STS);
 		}
-		if(Gdx.input.isKeyPressed(Keys.NUM_2))
+		/*if(Gdx.input.isKeyPressed(Keys.NUM_2))
 		{
 			game.BC.Stage(22);
+		    for (int i=0;i<game.BC.getRow();i++)
+			    System.out.println(game.BC.getBitRow(i));
 			game.setScreen(game.INS);
 		}
 		if(Gdx.input.isKeyPressed(Keys.NUM_3))
 		{
 			game.BC.Stage(33);
+		    for (int i=0;i<game.BC.getRow();i++)
+			    System.out.println(game.BC.getBitRow(i));
 			game.setScreen(game.INS);
 		}
 		if(Gdx.input.isKeyPressed(Keys.NUM_4))
 		{
 			game.BC.Stage(44);
+		    for (int i=0;i<game.BC.getRow();i++)
+			    System.out.println(game.BC.getBitRow(i));
 			game.setScreen(game.INS);
 		}
 		if(Gdx.input.isKeyPressed(Keys.NUM_5))
 		{
 			game.BC.Stage(55);
+		    for (int i=0;i<game.BC.getRow();i++)
+			    System.out.println(game.BC.getBitRow(i));
 			game.setScreen(game.INS);
-		}
-		game.fpslog.log();
-		game.fpslog.log();
+		}*/
+		//game.fpslog.log();
 	}
  
 	@Override
@@ -70,6 +80,7 @@ public class MainMenuScreen implements Screen{
  
 	@Override
 	public void hide() {
+		//System.out.println("main menu hide");
 		Gdx.input.setInputProcessor(null);
 	}
  
